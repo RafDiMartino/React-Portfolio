3840fe8073415f127fea2d2af9ea1ab8a85de105
R4FFF <69420740+R4FFF@users.noreply.github.com>
2021-06-01 13:55:33 +0100

    layout tests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Footer from './components/Footer';
 import HeroSection from './components/HeroSection';
 import Navbar from "./components/Navbar";
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Navbar />
       <HeroSection />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import { FaRegCopyright, FaReact } from "react-icons/fa"
 
 function Footer() {
     return (
-        <div>
-            
+        <div className="footer">
+            <p className="madeBy">&copy; 2021 by Raffaele Di Martino | </p>
+            <FaReact className="reactLogo"/>
         </div>
     )
 }
