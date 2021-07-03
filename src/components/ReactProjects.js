42de53c1076c8be5fc7845d6d7e0ca9923526872
R4FFF <69420740+R4FFF@users.noreply.github.com>
2021-07-03 12:19:16 +0100

    project added

--- src/components/ReactProjects.js
+++ src/components/ReactProjects.js
@@ -7,7 +7,8 @@ import Card from "./Card"
 import Quote from "./react-projects-imgs/quote-gen.png"
 import CounterApp from "./react-projects-imgs/counter-app.PNG"
 import ReactPortfolio from "./react-projects-imgs/react-portfolio.PNG"
-import WorkInProgress from "./react-projects-imgs/work-in- progress.jpg"
+import WorkInProgress from "./react-projects-imgs/WorkInProgress3.jpg"
+import TaskManager from "./react-projects-imgs/Task-Manager-dark.PNG"
 
 
 
@@ -21,8 +22,15 @@ function ReactProjects() {
             <div className="projects-container">
                 <Card
                     imageUrl={WorkInProgress}
-                    title="Task Manager"
+                    title="Weather App"
                     description="Work in Progress..."
+                    repoLink={<a className="card-anchor" href="#" target="_blank" rel="noreferrer" aria-label="github"><FaGithub className="github" /></a>}
+                    btn={<a className="card-anchor" href="#" target="_blank" rel="noreferrer">View project</a>}
+                />
+                <Card
+                    imageUrl={TaskManager}
+                    title="Task Manager"
+                    description="In this Progressive Web App I used Context for state management and Material UI"
                     repoLink={<a className="card-anchor" href="https://github.com/RafDiMartino/task-manager/tree/master" target="_blank" rel="noreferrer" aria-label="github"><FaGithub className="github" /></a>}
                     btn={<a className="card-anchor" href="http://rafdimartino.github.io/task-manager" target="_blank" rel="noreferrer">View project</a>}
                 />
