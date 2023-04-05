b22c62ba3f056a0482b9adad4a82cacd2702465c
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-04-05 14:51:51 +0100

    updating copyright date

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,7 @@ import { FaReact } from "react-icons/fa"
 function Footer() {
     return (
         <div className="footer">
-            <p className="madeBy">&copy; 2021 by Raffaele Di Martino | </p>
+            <p className="madeBy">&copy;2023 by Raffaele Di Martino | </p>
             <FaReact className="reactLogo"/>
         </div>
     )
