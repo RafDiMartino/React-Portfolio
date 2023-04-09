7ad4e1d3ed14007ce8eac5d10c18ed50a83c9658
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-04-09 20:40:22 +0100

    refactoring buttons-homepage

--- src/components/project-buttons/ProjectButtons.js
+++ src/components/project-buttons/ProjectButtons.js
@@ -8,9 +8,9 @@ function ProjectButtons() {
         <h2 className='project-title'>Projects</h2>
         <p>Explore all my projects by clicking on the buttons below</p>
         <div className='buttons-wrapper'>
-            <Link to="edx-bootcamp"><a className='button home-button'>edX Bootcamp</a></Link>
-            <Link to="react-projects"><a className='button home-button'>React Projects</a></Link>
-            <Link to="html-css-js-projects"><a className='button home-button'>HTML - CSS - JS</a></Link>
+            <Link to="edx-bootcamp"><button className='button home-button'>edX Bootcamp</button></Link>
+            <Link to="react-projects"><button className='button home-button'>React Projects</button></Link>
+            <Link to="html-css-js-projects"><button className='button home-button'>HTML - CSS - JS</button></Link>
         </div>
     </div>
   )
